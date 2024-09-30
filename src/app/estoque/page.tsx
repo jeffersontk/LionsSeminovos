743c75cb8c3c3c6af8afff28974bace600ad046a
jefferson <jscabral@firjan.com.br>
2024-09-30 00:26:19 -0300

    adicionando header e footer na pagina de estoque e finalizando detalhe no componente navigation

--- src/app/estoque/page.tsx
+++ src/app/estoque/page.tsx
@@ -1,7 +1,12 @@
+import { Footer } from "@/components/Organisms/Footer";
+import { Header } from "@/components/Organisms/Header";
+
 export default function Estoque() {
   return (
     <main>
+      <Header />
       <h1>Estoque</h1>
+      <Footer />
     </main>
   );
 }
--- src/components/Molecules/Navigation/index.tsx
+++ src/components/Molecules/Navigation/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { usePathname } from 'next/navigation';
 import { NavItem } from '@/components/Atoms/NavItem';
 
 export function Navigation() {
+  const pathname = usePathname()
+
   return (
     <div className='hidden lg:block'>
-      <nav
-        className={`lg:flex lg:flex-row flex flex-col gap-4 items-center`}
-      >
-        <NavItem label="Ofertas" href="/" active />
-        <NavItem label="Nosso Estoque" href="/estoque" />
+      <nav className='lg:flex lg:flex-row flex flex-col gap-4 items-center'>
+        <NavItem label="Ofertas" href="/" active={pathname === '/'} />
+        <NavItem label="Nosso Estoque" href="/estoque" active={pathname === '/estoque'} />
       </nav>
     </div>
   );
